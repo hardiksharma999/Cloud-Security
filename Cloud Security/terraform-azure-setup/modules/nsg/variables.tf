5387e85175d426ed634dd38666bcfce302cb524e
hardiksharma999 <hardiksharma15jan@gmail.com>
2024-10-05 18:46:06 +0530

    Add file

---
+++ Cloud Security/terraform-azure-setup/modules/nsg/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "The location for the NSG."
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the Network Security Group."
+  type        = string
+}
